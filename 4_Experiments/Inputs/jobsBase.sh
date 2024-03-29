6856a962d8d8fac4c7696432e961089e7ba0b689
PedroFernandes64 <pedrohenrique.phfs@gmail.com>
2024-03-29 16:35:46 +0100

    bigbig

--- 4_Experiments/Inputs/jobsBase.sh
+++ 4_Experiments/Inputs/jobsBase.sh
@@ -2,7 +2,7 @@
  
 #================= OPTIONS (s'applique à chaque job du tableau) =========================
 #SBATCH --array=0-14             # création d'un tableau de 15 jobs indicés de 0 à 14
-#SBATCH --partition=normal       # choix de la partition
+#SBATCH --partition=court        # choix de la partition
 #SBATCH --ntasks=1               # chaque job possède une seule task
 #SBATCH --cpus-per-task=2        # une task nécessite 2 CPU
 #SBATCH --mem-per-cpu=8192       # 8 Go de RAM par CPU
